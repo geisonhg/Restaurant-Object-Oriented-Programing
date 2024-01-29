86c8d04beae14f38340809f40ffd9b5bb9f5e0d6
geisonhg <67343209+geisonhg@users.noreply.github.com>
2024-01-29 15:14:05 +0000

    first commit

---
+++ src/Models/Order.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+import java.util.Date;
+
+/**
+ *
+ * @author geiso
+ */
+public class Order {
+    Date date = new Date();
+    
+}
---
+++ src/restaurantsells/RestaurantSells.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restaurantsells;
+
+import java.util.Date;
+
+/**
+ *
+ * @author geiso
+ */
+public class RestaurantSells {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Date date = new Date();
+
+      // display time and date using toString()
+      System.out.println(date.toString());
+    }
+    
+}
